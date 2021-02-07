2bc3e0adb69e23f02ea213998b9aeebae31474c9
Anastasia Boshniakovich <a.boshnyakovich@gmail.com>
2021-02-07 23:50:31 +0300

    added tests

--- pkg/parser/habr_test.go
+++ pkg/parser/habr_test.go
@@ -2,22 +2,28 @@ package parser
 
 import (
 	"github.com/boshnyakovich/news-aggregator/internal/models"
+	"github.com/geziyor/geziyor"
+	"github.com/geziyor/geziyor/export"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"net/http"
 	"testing"
+	"time"
+
+	"html/template"
 )
 
 type TestExporter struct {
 	news []models.HabrNews
 }
 
-func (t *TestExporter) Export (c chan interface{}) {
+func (t *TestExporter) Export(c chan interface{}) {
 	for data := range c {
 		t.news = append(t.news, data.(models.HabrNews))
 	}
 }
 
-func reaHabrFile() []byte {
+func readHabrFile() []byte {
 	data, _ := ioutil.ReadFile("test_files/habr.html")
 	return data
 }
@@ -27,14 +33,43 @@ func TestHabrParser_MakeUrl(t *testing.T) {
 	url, err := parser.MakeUrl(models.HabrCriteria{})
 
 	assert.NoError(t, err)
-	assert.Equal(t,"https://habr.com/ru/news/", url)
+	assert.Equal(t, "https://habr.com/ru/news/", url)
 
 	url, err = parser.MakeUrl(models.HabrCriteria{
 		Articles: true,
-		All: true,
-		Rating: uint64(models.TEN),
+		All:      true,
+		Rating:   uint64(models.TEN),
 	})
 
 	assert.NoError(t, err)
-	assert.Equal(t,"https://habr.com/ru/all/top10/", url)
+	assert.Equal(t, "https://habr.com/ru/all/top10/", url)
+}
+
+func TestHabrParser_Parse(t *testing.T) {
+
+	testExporter := &TestExporter{}
+
+	go func () {
+		http.HandleFunc("/test", habrPageTemplate)
+		err := http.ListenAndServe("localhost:9098", nil)
+		assert.NoError(t, err)
+	}()
+
+	parser := &HabrParser{}
+	gz := geziyor.NewGeziyor(&geziyor.Options{
+		StartURLs: []string{"http://localhost:9098/test"},
+		ParseFunc: parser.parse,
+		Exporters: []export.Exporter{testExporter},
+	})
+
+	time.Sleep(time.Second * 10)
+
+	gz.Start()
+
+	assert.NotEmpty(t, testExporter.news)
+}
+
+func habrPageTemplate(w http.ResponseWriter, r *http.Request) {
+	var homeTemplate = template.Must(template.New("").Parse(string(readHabrFile())))
+	homeTemplate.Execute(w, "")
 }
